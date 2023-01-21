6438a91308580b99d57653fb51c72a37750b7493
EyadTamer999 <eyadhasssan2002@gmail.com>
2023-01-21 23:11:50 +0200

    Added Query with states, so we can start searching using the search bar

--- src/components/ListContacts.js
+++ src/components/ListContacts.js
@@ -1,24 +1,39 @@
 import PropTypes from "prop-types";
-const ListContacts = ({balabeezo, onDeleteContact}) => {
-    return (<ol className="contact-list">
-        {balabeezo.map((contact) => (<li key={contact.id} className="contact-list-item">
-            <div className="contact-avatar"
-                 style={{
-                     backgroundImage: `url(${contact.avatarURL})`,
+import {useState} from "react";
 
-                 }}>
+const ListContacts = ({balabeezo, onDeleteContact}) => {
+
+    const[query, setQuery] = useState("");
+    const updateQuery = (input) => {
+        setQuery(input.trim());
+    }
+
+    return (
+        <div className="list-contacts">
+            <div className="list-contacts-top">
+                <input className="search-contacts" type="text" placeholder="Search" value={query} onChange={(event) => updateQuery(event.target.value)}/>
             </div>
-            <div className="contact-details">
-                <p>
-                    {contact.name}
-                </p>
-                <p>
-                    {contact.handle}
-                </p>
-            </div>
-            <button className="contact-remove" onClick={() => onDeleteContact(contact)}>Remove</button>
-        </li>))}
-    </ol>);
+            <ol className="contact-list">
+                {balabeezo.map((contact) => (<li key={contact.id} className="contact-list-item">
+                    <div className="contact-avatar"
+                         style={{
+                             backgroundImage: `url(${contact.avatarURL})`,
+
+                         }}>
+                    </div>
+                    <div className="contact-details">
+                        <p>
+                            {contact.name}
+                        </p>
+                        <p>
+                            {contact.handle}
+                        </p>
+                    </div>
+                    <button className="contact-remove" onClick={() => onDeleteContact(contact)}>Remove</button>
+                </li>))}
+            </ol>
+        </div>
+    );
 };
 
 ListContacts.protoTypes = {
