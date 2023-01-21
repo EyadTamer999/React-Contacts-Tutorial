63fc036580c440c52ae5491b5ff6709cef94b5fd
EyadTamer999 <eyadhasssan2002@gmail.com>
2023-01-21 23:37:18 +0200

    Finalized Search bar!

--- src/components/ListContacts.js
+++ src/components/ListContacts.js
@@ -3,18 +3,39 @@ import {useState} from "react";
 
 const ListContacts = ({balabeezo, onDeleteContact}) => {
 
-    const[query, setQuery] = useState("");
+    const [query, setQuery] = useState("");
     const updateQuery = (input) => {
         setQuery(input.trim());
-    }
+    };
+
+    const clearQuery = () => {
+        updateQuery("");
+    };
+
+    const showingContacts = query === "" ? balabeezo : balabeezo.filter((c) => c.name.toLowerCase().includes(query.toLowerCase()));
 
     return (
         <div className="list-contacts">
             <div className="list-contacts-top">
-                <input className="search-contacts" type="text" placeholder="Search" value={query} onChange={(event) => updateQuery(event.target.value)}/>
+                <input className="search-contacts" type="text" placeholder="Search" value={query}
+                       onChange={(event) => updateQuery(event.target.value)}/>
             </div>
+
+            {
+                showingContacts.length !== balabeezo.length && (
+                    <div className="showing-contacts">
+                        <span>
+                            Now Showing {showingContacts.length} of {balabeezo.length}
+                        </span>
+                        <button onClick={clearQuery}>
+                            Show all
+                        </button>
+                    </div>
+                )
+            }
+
             <ol className="contact-list">
-                {balabeezo.map((contact) => (<li key={contact.id} className="contact-list-item">
+                {showingContacts.map((contact) => (<li key={contact.id} className="contact-list-item">
                     <div className="contact-avatar"
                          style={{
                              backgroundImage: `url(${contact.avatarURL})`,
