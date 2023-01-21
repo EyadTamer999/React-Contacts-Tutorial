67b44a3cda1b5049adbbe1884e6c1efad4a4e175
EyadTamer999 <eyadhasssan2002@gmail.com>
2023-01-21 22:22:08 +0200

    Created contact state, added some comments

--- src/components/App.js
+++ src/components/App.js
@@ -1,27 +1,40 @@
 import "../css/App.css";
 import ListContacts from "./ListContacts";
+import {useState} from "react";
 
 const App = () => {
-    const contacts = [
-        {
-            id: "karen",
-            name: "Karen Isgrigg",
-            handle: "karen_isgrigg",
-            avatarURL: "http://localhost:5001/karen.jpg",
-        },
-        {
-            id: "richard",
-            name: "Richard Kalehoff",
-            handle: "richardkalehoff",
-            avatarURL: "http://localhost:5001/richard.jpg",
-        },
-        {
-            id: "tyler",
-            name: "Tyler McGinnis",
-            handle: "tylermcginnis",
-            avatarURL: "http://localhost:5001/tyler.jpg",
-        },
-    ];
+    //create function that allows us to set state for later contact manipulation like remove
+    const [contacts, setContacts] = useState(
+        //initial state is basically the state that we start with.
+        [
+            {
+                id: "karen",
+                name: "Karen Isgrigg",
+                handle: "karen_isgrigg",
+                avatarURL: "http://localhost:5001/karen.jpg",
+            },
+            {
+                id: "richard",
+                name: "Richard Kalehoff",
+                handle: "richardkalehoff",
+                avatarURL: "http://localhost:5001/richard.jpg",
+            },
+            {
+                id: "tyler",
+                name: "Tyler McGinnis",
+                handle: "tylermcginnis",
+                avatarURL: "http://localhost:5001/tyler.jpg",
+            },
+        ]
+    );
+
+    const removeContact = (contact) => {
+        setContacts(contact.filter(rm => rm.id !== contact.id))
+    }
+
+
+    //returns a div that has our React component with the passed arguments contacts that we created in App.js
+
     return (
         <div>
             <ListContacts contacts={contacts}/>
